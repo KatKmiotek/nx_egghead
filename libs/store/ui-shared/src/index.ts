5fb5f0f6e3b04112ccd881fbb2418e66d77bb64e
KatKmiotek <katarzyna.kmiotek@yahoo.com>
2022-02-17 20:03:42 +0000

    egghead nx course

---
+++ libs/store/ui-shared/src/index.ts
@@ -0,0 +1,2 @@
+export * from './lib/header/header';
+export * from '@nxegghead/store/ui-shared';
---
+++ libs/store/util-formatter/src/lib/store-util-formatter.spec.ts
@@ -0,0 +1,7 @@
+import { storeUtilFormatter } from './store-util-formatter';
+
+describe('storeUtilFormatter', () => {
+  it('should work', () => {
+    expect(storeUtilFormatter()).toEqual('store-util-formatter');
+  });
+});
